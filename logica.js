ed4bb5ba6de522f46224503ecbbf84e8d2637675
23albania <143647482+23albania@users.noreply.github.com>
2024-12-12 22:28:44 -0300

    Web apriori v1.1
    Ajustes de funcionalidad y esteticos a pedido

--- logica.js
+++ logica.js
@@ -1,3 +1,17 @@
+//Slowscroll al uso de botones para redirigir a seccion
+
+document.getElementById("btn-escribenos").addEventListener("click", function() {
+    document.getElementById("contactanos").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("btn-sobre").addEventListener("click", function() {
+    document.getElementById("servicios").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("btn-choice").addEventListener("click", function() {
+    document.getElementById("footer").scrollIntoView({ behavior: "smooth" });
+});
+
 // Seleccionar el textarea y el contador
 const textarea = document.getElementById('cuentanos');
 const charCounter = document.getElementById('char-counter');
@@ -18,3 +32,19 @@ textarea.addEventListener('input', () => {
         charCounter.textContent = `${maxChars}/${maxChars} caracteres`;
     }
 });
+
+const words = ["Apriori a","Antes de"];
+let currentWordIndex = 0;
+
+setInterval(() => {
+    const fadeWordElement = document.getElementById("fade-word");
+    fadeWordElement.style.opacity = "0";
+
+    setTimeout(() => {
+        currentWordIndex = (currentWordIndex + 1) % words.length;
+        fadeWordElement.textContent = words[currentWordIndex];
+        fadeWordElement.style.opacity = "1";
+    }, 1000); // Tiempo sincronizado con la transición CSS
+}, 3000); // Intervalo total (igual al tiempo total de la animación)
+
+
